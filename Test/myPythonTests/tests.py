5ec4b2c69819c921b8b445935010023ce5b1be54
Julio Alvarez <julio.alvarez@ericsson.com>
2018-08-10 13:24:02 +0200

    Modified

--- Test/myPythonTests/tests.py
+++ Test/myPythonTests/tests.py
@@ -11,12 +11,7 @@ import pandas as pd
 print ("hola fucker !!\n")
 df = pd.read_excel('C:\\Users\\ejulalv\\Desktop\\Trabajo\\ejemplos.xlsx',sheet_name='Sheet1')
 
-print (df.sort_values(['Nombre'].plot(kind="barh")))
-'''print (df.loc[0]) 
-df.set_index('Nombre',inplace=True)'''
- 
-#print 
-#print (df.loc[0]) 
+print (df.sort_values(['Nombre']))
 
 if __name__ == '__main__':
     pass
